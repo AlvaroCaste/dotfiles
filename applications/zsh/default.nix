cdff3a465b90716996ec6df7cc1140ac80e3db80
Álvaro Castellanos López <alvaro.castellanos.lopez@gmail.com>
2022-03-25 15:32:21 +0100

    add p10k config

--- applications/zsh/default.nix
+++ applications/zsh/default.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 {
 
   home.packages = [ pkgs.zsh pkgs.zsh-powerlevel10k ];
@@ -17,6 +17,11 @@
         src = pkgs.zsh-powerlevel10k;
         file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
       }
+      {
+        name = "powerlevel10k-config";
+        src = lib.cleanSource ./p10k-config;
+        file = "p10k.zsh";
+      }
     ];
   };
 
@@ -32,5 +37,4 @@
       "virtualenvwrapper"
     ];
   };
-
 }
