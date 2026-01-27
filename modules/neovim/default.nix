8cac31f0d50027becc0d15e809fd22084b343fc9
Álvaro Castellanos López <alvaro.castellanos.lopez@gmail.com>
2026-01-27 15:27:00 +0100

    Modernize Neovim config

--- modules/neovim/default.nix
+++ modules/neovim/default.nix
@@ -1,17 +1,17 @@
 { config, pkgs, ... }:
 {
-
-  home.file.".vimrc".source = ./vimrc;
-
   programs.neovim = {
     enable = true;
     viAlias = true;
     vimAlias = true;
-    extraConfig = ''
-        set runtimepath^=~/.vim runtimepath+=~/.vim/after
-      let &packpath=&runtimepath
-      source ~/.vimrc
-    '';
+    plugins = with pkgs.vimPlugins; [
+      lualine-nvim # statusline
+      nvim-tree-lua # file tree explorer
+      nvim-web-devicons # file icons for tree/statusline
+      gitsigns-nvim # git diff signs in the gutter
+      indent-blankline-nvim # indent guides
+      nvim-treesitter # treesitter (no markdown parsers)
+    ];
+    initLua = builtins.readFile ./init.lua;
   };
-
 }
---
+++ modules/neovim/init.lua
@@ -0,0 +1,54 @@
+require("lualine").setup({}) -- statusline
+require("nvim-tree").setup({}) -- file tree
+require("gitsigns").setup({}) -- git diff signs
+require("ibl").setup({}) -- indent guides
+local ts_ok, ts = pcall(require, "nvim-treesitter.configs")
+if ts_ok then
+  ts.setup({
+    highlight = { enable = true },
+  })
+end -- treesitter highlighting (guarded if plugin not on runtimepath)
+
+vim.g.mapleader = "," -- leader key
+vim.o.mouse = "a" -- enable mouse in all modes
+vim.o.clipboard = "unnamedplus,unnamed" -- system clipboard and primary selection
+
+vim.o.expandtab = true -- tabs -> spaces
+vim.o.smarttab = true -- smarter tab handling
+vim.o.tabstop = 2 -- visible tab width
+vim.o.softtabstop = 2 -- insert-mode tab width
+vim.o.shiftwidth = 2 -- indent width
+vim.o.incsearch = true -- incremental search
+vim.o.ignorecase = true -- case-insensitive search
+vim.o.hlsearch = true -- highlight matches
+vim.o.number = true -- absolute line numbers
+vim.o.relativenumber = true -- relative line numbers
+vim.o.background = "dark" -- colorscheme expects dark background
+vim.o.cursorline = true -- highlight current line
+vim.o.wildmenu = true -- command-line completion menu
+vim.o.showmatch = true -- highlight matching brackets
+vim.o.splitright = true -- vertical splits open on the right
+vim.o.foldenable = false -- keep folds open by default
+vim.o.foldlevelstart = 10 -- open most folds if folding enabled
+
+local map = vim.keymap.set -- keymap helper
+map("i", "<Up>", "<Nop>") -- disable arrow keys in insert mode
+map("i", "<Down>", "<Nop>")
+map("i", "<Left>", "<Nop>")
+map("i", "<Right>", "<Nop>")
+
+map("n", "<Space>", ":") -- Space -> :
+map("v", "<Space>", ":")
+
+map("n", "<Tab>", "%") -- Tab -> matching bracket
+map("v", "<Tab>", "%")
+
+map("n", "<C-h>", "<C-w>h") -- split navigation
+map("n", "<C-j>", "<C-w>j")
+map("n", "<C-k>", "<C-w>k")
+map("n", "<C-l>", "<C-w>l")
+
+map({ "n", "v" }, "d", '"_d') -- delete without yanking
+map("n", "x", '"_x')
+map("n", "X", '"_X')
+map("n", "D", '"_D')
